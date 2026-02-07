460e69d68b368766344ac4229c598a9935a3eb0d
Bako <bakoabdi77@gmail.com>
2026-02-08 00:24:56 +0500

    FINAL VERSION: Full DApp integration. Implemented Crowdfunding, ERC-20 Burning, and ERC-721 NFT Minting. Fixed UI navigation and persistent inventory.

--- contracts/KittyCafe.sol
+++ contracts/KittyCafe.sol
@@ -1,16 +1,29 @@
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
 contract KittyToken is ERC20 {
+    address public minter;
+    constructor() ERC20("KittyToken", "KITTY") { minter = msg.sender; }
+    function mint(address to, uint256 amount) external { _mint(to, amount); }
+    function burn(uint256 amount) external {
+        _burn(msg.sender, amount);
+    }
+}
+
+contract KittyNFT is ERC721URIStorage {
+    uint256 public nextTokenId;
     address public owner;
 
-    constructor() ERC20("KittyToken", "KITTY") {
-        owner = msg.sender;
-    }
+    constructor() ERC721("KittyCafeItem", "KITEM") { owner = msg.sender; }
 
-    function mint(address to, uint256 amount) external {
-        _mint(to, amount);
+    function mintItem(address player, string memory tokenURI) external returns (uint256) {
+        uint256 tokenId = nextTokenId;
+        _safeMint(player, tokenId);
+        _setTokenURI(tokenId, tokenURI);
+        nextTokenId++;
+        return tokenId;
     }
 }
 
@@ -27,7 +40,6 @@ contract KittyCrowdfunding {
     KittyToken public rewardToken;
     mapping(uint256 => Campaign) public campaigns;
     uint256 public campaignCount;
-    
     uint256 public constant REWARD_RATE = 1000;
 
     constructor(address _tokenAddress) {
@@ -43,9 +55,7 @@ contract KittyCrowdfunding {
     function contribute(uint256 _id) public payable {
         Campaign storage c = campaigns[_id];
         require(block.timestamp < c.deadline, "Ended");
-        
         c.amountRaised += msg.value;
-
         uint256 reward = msg.value * REWARD_RATE;
         rewardToken.mint(msg.sender, reward);
     }
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -2,20 +2,25 @@ const hre = require("hardhat");
 
 async function main() {
   const [deployer] = await hre.ethers.getSigners();
-
-  console.log("Deploying contracts with the account:", deployer.address);
+  console.log("Deploying with:", deployer.address);
 
   const KittyToken = await hre.ethers.getContractFactory("KittyToken");
   const token = await KittyToken.deploy();
-  await token.waitForDeployment(); 
-  const tokenAddress = await token.getAddress();
-  console.log("KittyToken deployed to:", tokenAddress);
+  await token.waitForDeployment();
+  const tokenAddr = await token.getAddress();
+  console.log("KittyToken deployed to:", tokenAddr);
+
+  const KittyNFT = await hre.ethers.getContractFactory("KittyNFT");
+  const nft = await KittyNFT.deploy();
+  await nft.waitForDeployment();
+  const nftAddr = await nft.getAddress();
+  console.log("KittyNFT deployed to:", nftAddr);
 
   const KittyCrowdfunding = await hre.ethers.getContractFactory("KittyCrowdfunding");
-  const crowdfunding = await KittyCrowdfunding.deploy(tokenAddress);
+  const crowdfunding = await KittyCrowdfunding.deploy(tokenAddr);
   await crowdfunding.waitForDeployment();
-  const cfAddress = await crowdfunding.getAddress();
-  console.log("KittyCrowdfunding deployed to:", cfAddress);
+  const cfAddr = await crowdfunding.getAddress();
+  console.log("KittyCrowdfunding deployed to:", cfAddr);
 }
 
 main().catch((error) => {
